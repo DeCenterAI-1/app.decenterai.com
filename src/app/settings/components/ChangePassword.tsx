e3191840a28c420aeba28caaede84267b68c9b78
codypharm <williamikeji@gmail.com>
2024-10-17 17:53:14 +0100

    feat: password change modal

---
+++ src/app/settings/components/ChangePassword.tsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
+import { VscSearch } from 'react-icons/vsc'
+import Image from 'next/image'
+import Input from '@/app/components/Input'
+import { AiOutlineEye } from 'react-icons/ai'
+import { PiEyeClosedLight } from 'react-icons/pi'
+import Button from '@/app/components/Button'
+
+function ChangePassword({ setShowModal }: { setShowModal: (state: boolean) => void }) {
+  const myImageLoader = ({ src }) => {
+    return src
+  }
+  const [view, setView] = useState<boolean>(false)
+  const [view1, setView1] = useState<boolean>(false)
+  const [view2, setView2] = useState<boolean>(false)
+
+  return (
+    <div
+      className={`absolute inset-0 flex justify-center items-center  bg-primary_13/70  `}>
+      <form className="w-1/3   h-1/2 bg-primary_10 rounded-xl z-50 flex flex-col gap-3  text-primary_1 justify-center px-6">
+        <div className="h-[20%]  flex items-center ">
+          <h3 className="font-logirentBold text-2xl">Change Password</h3>
+        </div>
+
+        <div className="h-[20%] flex flex-col gap-2">
+          <label htmlFor="pwd1" className="font-archivo text-primary_1 text-sm">
+            Current password
+          </label>
+
+          <div className="flex flex-grow border border-primary_7 px-4 rounded-xl relative">
+            <div className="absolute top-0 right-4 h-full flex items-center  text-primary_1">
+              {!view && (
+                <AiOutlineEye
+                  size={20}
+                  className="cursor-pointer"
+                  onClick={() => setView(!view)}
+                />
+              )}
+              {view && (
+                <PiEyeClosedLight
+                  size={20}
+                  className="cursor-pointer"
+                  onClick={() => setView(!view)}
+                />
+              )}
+            </div>
+            <Input
+              type={view ? 'text' : 'password'}
+              placeholder="Enter your secure password here"
+              className="bg-transparent pr-12"
+            />
+          </div>
+        </div>
+        <div className="h-[20%] flex flex-col gap-2">
+          <label htmlFor="pwd1" className="font-archivo text-primary_1 text-sm">
+            New password
+          </label>
+
+          <div className="flex flex-grow border border-primary_7 px-4 rounded-xl relative">
+            <div className="absolute top-0 right-4 h-full flex items-center  text-primary_1">
+              {!view1 && (
+                <AiOutlineEye
+                  size={20}
+                  className="cursor-pointer"
+                  onClick={() => setView1(!view1)}
+                />
+              )}
+              {view1 && (
+                <PiEyeClosedLight
+                  size={20}
+                  className="cursor-pointer"
+                  onClick={() => setView1(!view1)}
+                />
+              )}
+            </div>
+            <Input
+              type={view1 ? 'text' : 'password'}
+              placeholder="Enter your new password here"
+              className="bg-transparent pr-12"
+            />
+          </div>
+        </div>
+        <div className="h-[20%] flex flex-col gap-2">
+          <label htmlFor="pwd1" className="font-archivo text-primary_1 text-sm">
+            Repeat password
+          </label>
+
+          <div className="flex flex-grow border border-primary_7 px-4 rounded-xl relative">
+            <div className="absolute top-0 right-4 h-full flex items-center  text-primary_1">
+              {!view2 && (
+                <AiOutlineEye
+                  size={20}
+                  className="cursor-pointer"
+                  onClick={() => setView2(!view2)}
+                />
+              )}
+              {view2 && (
+                <PiEyeClosedLight
+                  size={20}
+                  className="cursor-pointer"
+                  onClick={() => setView2(!view2)}
+                />
+              )}
+            </div>
+            <Input
+              type={view2 ? 'text' : 'password'}
+              placeholder="Repeat password "
+              className="bg-transparent pr-12"
+            />
+          </div>
+        </div>
+
+        <div className="h-[20%] flex  gap-4 justify-end items-center">
+          <Button
+            onClick={() => setShowModal(false)}
+            className="bg-primary_12 px-6 h-fit py-3">
+            Cancel
+          </Button>
+          <Button className="bg-primary_11 text-primary_7 px-6 h-fit py-3">
+            Save changes
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ChangePassword
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { AppLayout } from '../components/appLayout'
+import Button from '../components/Button'
+import { RxExit } from 'react-icons/rx'
+import { LuUser2 } from 'react-icons/lu'
+import { VscLock } from 'react-icons/vsc'
+import { GoBell } from 'react-icons/go'
+import ChangePassword from './components/ChangePassword'
+
+export default function Home() {
+  const [showModal, setShowModal] = useState<boolean>(false)
+
+  return (
+    <main className="bg-primary_13 relative">
+      <AppLayout>
+        {showModal && <ChangePassword setShowModal={setShowModal} />}
+        <section className="h-full w-full  text-primary_1 pt-20 pb-6 flex  gap-8">
+          <div className="h-full   w-[80%] overflow-y-auto px-10  flex flex-col gap-8 ">
+            <div className="w-full border border-primary_7 h-16 rounded-xl flex space-x-4 items-center  text-primary_7 px-4 cursor-pointer">
+              <LuUser2 size={20} />
+              <p className="font-semibold text-primary_1">Profile</p>
+            </div>
+            <div
+              onClick={() => setShowModal(true)}
+              className="w-full border border-primary_7 h-16 rounded-xl flex space-x-4 items-center  text-primary_7 px-4 cursor-pointer">
+              <VscLock size={20} />
+              <p className="font-semibold text-primary_1">Security</p>
+            </div>
+            <div className="w-full border border-primary_7 h-16 rounded-xl flex space-x-4 items-center  text-primary_7 px-4 cursor-pointer">
+              <GoBell size={20} />
+              <p className="font-semibold text-primary_1">Notification</p>
+            </div>
+          </div>
+          <div className="h-full w-[20%]">
+            <Button className="w-36 justify-center py-4">
+              <RxExit size={20} /> Log out
+            </Button>
+          </div>
+        </section>
+      </AppLayout>
+    </main>
+  )
+}
