bce29284510cf299dc45e3eb2b19b3a7550e187b
codypharm <williamikeji@gmail.com>
2024-10-17 18:30:13 +0100

    feat: notifications

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -7,14 +7,19 @@ import { LuUser2 } from 'react-icons/lu'
 import { VscLock } from 'react-icons/vsc'
 import { GoBell } from 'react-icons/go'
 import ChangePassword from './components/ChangePassword'
+import NotificationSettings from './components/NotificationSettings'
 
 export default function Home() {
   const [showModal, setShowModal] = useState<boolean>(false)
+  const [showNotificationModal, setShowNotificationModal] = useState<boolean>(true)
 
   return (
     <main className="bg-primary_13 relative">
       <AppLayout>
         {showModal && <ChangePassword setShowModal={setShowModal} />}
+        {showNotificationModal && (
+          <NotificationSettings setShowNotificationModal={setShowNotificationModal} />
+        )}
         <section className="h-full w-full  text-primary_1 pt-20 pb-6 flex  gap-8">
           <div className="h-full   w-[80%] overflow-y-auto px-10  flex flex-col gap-8 ">
             <div className="w-full border border-primary_7 h-16 rounded-xl flex space-x-4 items-center  text-primary_7 px-4 cursor-pointer">
@@ -27,7 +32,9 @@ export default function Home() {
               <VscLock size={20} />
               <p className="font-semibold text-primary_1">Security</p>
             </div>
-            <div className="w-full border border-primary_7 h-16 rounded-xl flex space-x-4 items-center  text-primary_7 px-4 cursor-pointer">
+            <div
+              onClick={() => setShowNotificationModal(true)}
+              className="w-full border border-primary_7 h-16 rounded-xl flex space-x-4 items-center  text-primary_7 px-4 cursor-pointer">
               <GoBell size={20} />
               <p className="font-semibold text-primary_1">Notification</p>
             </div>
